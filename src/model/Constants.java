0f26bb0913fd7941992dbb58d581e3236214c2f1
mmtec <maior@mminc.de>
2016-06-25 11:40:36 +0200

    third try

---
+++ src/model/Constants.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Constants {
+
+	public static final int GAME_WIDTH = 800;
+	public static final int GAME_HEIGHT = 800;
+	
+	public static final int BALL_X = GAME_WIDTH / 2 - 10;
+	public static final int BALL_Y = GAME_HEIGHT / 3 + 20;
+	public static final int BALL_WIDTH = 20;
+	public static final int BALL_HEIGHT = 20;
+	
+	public static final int BAT_X = GAME_WIDTH / 2 - 80;
+	public static final int BAT_Y = GAME_HEIGHT - 40;
+	public static final int BAT_WIDTH = 160;
+	public static final int BAT_HEIGHT = 20;
+	
+	public static final int BRICK_WIDTH = 80;
+	public static final int BRICK_HEIGHT = 30;
+	
+	public Constants() {
+		
+	}
+}
